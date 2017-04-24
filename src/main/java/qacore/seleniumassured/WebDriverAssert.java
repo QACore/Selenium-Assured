bba5301f4e47a0d7f36695051ee7446d03035d7b
Leonardo Carmona <leonardo.carmona@cwi.com.br>
2017-04-23 22:55:52 -0300

    Inclusion of WebDriverAssert class

---
+++ src/main/java/qacore/seleniumassured/WebDriverAssert.java
@@ -0,0 +1,21 @@
+package qacore.seleniumassured;
+
+/**
+ * 
+ * 
+ * @author Leonardo Carmona da Silva
+ *         <li><a href="https://br.linkedin.com/in/l3ocarmona">https://br.linkedin.com/in/l3ocarmona</a></li>
+ *         <li><a href="https://github.com/LeoCarmona">https://github.com/LeoCarmona</a></li>
+ *         </ul>
+ *
+ */
+public final class WebDriverAssert {
+
+	/**
+	 * Private constructor (static class)
+	 */
+	private WebDriverAssert() {
+
+	}
+
+}
