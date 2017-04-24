9dfe270ab4a2ceeb55cf5a98a1293782eefc3fa1
Leonardo Carmona <leonardo.carmona@cwi.com.br>
2017-04-23 23:21:56 -0300

    Inclusion of WebDriverAssertTest

---
+++ src/test/java/qacore/seleniumassured/WebDriverAssertTest.java
@@ -0,0 +1,22 @@
+package qacore.seleniumassured;
+
+import org.junit.Test;
+
+/**
+ * 
+ * 
+ * @author Leonardo Carmona da Silva
+ *         <ul>
+ *         <li><a href="https://br.linkedin.com/in/l3ocarmona">https://br.linkedin.com/in/l3ocarmona</a></li>
+ *         <li><a href="https://github.com/LeoCarmona">https://github.com/LeoCarmona</a></li>
+ *         </ul>
+ *
+ */
+public class WebDriverAssertTest {
+	
+	@Test
+	public void passTest() {
+		
+	}
+
+}
