90cbc760ebb3be9f3569eb60daa480ac22f5750c
Leonardo Carmona <leonardo.carmona@cwi.com.br>
2017-04-24 00:20:06 -0300

    WebDriverAssert javadoc

--- src/main/java/qacore/seleniumassured/WebDriverAssert.java
+++ src/main/java/qacore/seleniumassured/WebDriverAssert.java
@@ -6,13 +6,23 @@ import static org.junit.Assert.assertNotEquals;
 import org.openqa.selenium.WebDriver;
 
 /**
- * 
+ * A set of assertion methods useful for writing {@link WebDriver} tests. Only failed assertions are recorded. These methods can be used directly: <code>WebDriverAssert.assertTitleEquals(...)</code>, however, they read better if they are referenced through static import:
+ *
+ * <pre>
+ * import static qacore.seleniumassured.WebDriverAssert.*;
+ *    ...
+ *    assertTitleEquals(...);
+ * </pre>
  * 
  * @author Leonardo Carmona da Silva
  *         <ul>
  *         <li><a href="https://br.linkedin.com/in/l3ocarmona">https://br.linkedin.com/in/l3ocarmona</a></li>
  *         <li><a href="https://github.com/LeoCarmona">https://github.com/LeoCarmona</a></li>
  *         </ul>
+ *
+ * @see AssertionError
+ * 
+ * @since 1.0.0
  *
  */
 public final class WebDriverAssert {
