848c01ba436c3143b8653183a119c1ffc42ba5f8
Leonardo Carmona <leonardo.carmona@cwi.com.br>
2017-04-25 14:23:35 -0300

    Inclusion of all title assertions

--- src/main/java/qacore/seleniumassured/WebDriverAssert.java
+++ src/main/java/qacore/seleniumassured/WebDriverAssert.java
@@ -82,13 +82,69 @@ public final class WebDriverAssert {
 	}
 
 	/**
+	 * Tests if <em>title</em> is empty.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 * 
+	 * @param trim
+	 *            if <code>true</code>, all leading and trailing whitespace removed. Otherwise, false.
+	 */
+	public static void assertTitleIsEmpty(WebDriver driver, boolean trim) {
+		String title = driver.getTitle();
+
+		if (trim)
+			title = title.trim();
+
+		assertTrue("The title is empty", title.isEmpty());
+	}
+
+	/**
+	 * Tests if <em>title</em> is empty.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 */
+	public static void assertTitleIsEmpty(WebDriver driver) {
+		assertTitleIsEmpty(driver, false);
+	}
+
+	/**
+	 * Tests if <em>title</em> is not empty.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 * 
+	 * @param trim
+	 *            if <code>true</code>, all leading and trailing whitespace removed. Otherwise, false.
+	 */
+	public static void assertTitleIsNotEmpty(WebDriver driver, boolean trim) {
+		String title = driver.getTitle();
+
+		if (trim)
+			title = title.trim();
+
+		assertFalse("The title <" + title + "> is not empty", title.isEmpty());
+	}
+
+	/**
+	 * Tests if <em>title</em> is not empty.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 */
+	public static void assertTitleIsNotEmpty(WebDriver driver) {
+		assertTitleIsNotEmpty(driver, false);
+	}
+
+	/**
 	 * Tests if the substring of <em>title</em> beginning at the specified index starts with the specified <code>prefix</code>.
 	 * 
 	 * @param driver
 	 *            the <code>driver</code> where will be checked
 	 * 
 	 * @param prefix
-	 *            the <em>title</em> prefix
+	 *            the <em>title</em> <code>prefix</code>
 	 * 
 	 * @param toffset
 	 *            where to begin looking in this string
@@ -106,10 +162,42 @@ public final class WebDriverAssert {
 	 *            the <code>driver</code> where will be checked
 	 * 
 	 * @param prefix
-	 *            the <em>title</em> prefix
+	 *            the <em>title</em> <code>prefix</code>
 	 */
 	public static void assertTitleStartsWith(WebDriver driver, String prefix) {
 		assertTitleStartsWith(driver, prefix, 0);
+	}
+
+	/**
+	 * Tests if the substring of <em>title</em> beginning at the specified index starts with the specified <code>prefix</code> ignoring case.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 * 
+	 * @param prefix
+	 *            the <em>title</em> <code>prefix</code>
+	 * 
+	 * @param toffset
+	 *            where to begin looking in this string
+	 */
+	public static void assertTitleStartsWithIgnoreCase(WebDriver driver, String prefix, int toffset) {
+		String title = driver.getTitle().toLowerCase();
+		prefix = prefix.toLowerCase();
+
+		assertTrue("The title <" + title + "> doesn't start with <" + prefix + "> at index " + toffset, title.startsWith(prefix, toffset));
+	}
+
+	/**
+	 * Tests if the substring of <em>title</em> beginning at the specified index starts with the specified <code>prefix</code> ignoring case.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 * 
+	 * @param prefix
+	 *            the <em>title</em> <code>prefix</code>
+	 */
+	public static void assertTitleStartsWithIgnoreCase(WebDriver driver, String prefix) {
+		assertTitleStartsWithIgnoreCase(driver, prefix, 0);
 	}
 
 	/**
@@ -119,7 +207,7 @@ public final class WebDriverAssert {
 	 *            the <code>driver</code> where will be checked
 	 * 
 	 * @param prefix
-	 *            the <em>title</em> prefix
+	 *            the <em>title</em> <code>prefix</code>
 	 * 
 	 * @param toffset
 	 *            where to begin looking in this string
@@ -137,10 +225,196 @@ public final class WebDriverAssert {
 	 *            the <code>driver</code> where will be checked
 	 * 
 	 * @param prefix
-	 *            the <em>title</em> prefix
+	 *            the <em>title</em> <code>prefix</code>
 	 */
 	public static void assertTitleDoesNotStartWith(WebDriver driver, String prefix) {
 		assertTitleDoesNotStartWith(driver, prefix, 0);
+	}
+
+	/**
+	 * Tests if the substring of <em>title</em> does not beginning at the specified index starts with the specified <code>prefix</code> ignoring case.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 * 
+	 * @param prefix
+	 *            the <em>title</em> <code>prefix</code>
+	 * 
+	 * @param toffset
+	 *            where to begin looking in this string
+	 */
+	public static void assertTitleDoesNotStartWithIgnoreCase(WebDriver driver, String prefix, int toffset) {
+		String title = driver.getTitle().toLowerCase();
+		prefix = prefix.toLowerCase();
+
+		assertFalse("The title <" + title + "> starts with <" + prefix + "> at index " + toffset, title.startsWith(prefix, toffset));
+	}
+
+	/**
+	 * Tests if the substring of <em>title</em> does not beginning at the specified index starts with the specified <code>prefix</code> ignoring case.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 * 
+	 * @param prefix
+	 *            the <em>title</em> <code>prefix</code>
+	 */
+	public static void assertTitleDoesNotStartWithIgnoreCase(WebDriver driver, String prefix) {
+		assertTitleDoesNotStartWith(driver, prefix, 0);
+	}
+
+	/**
+	 * Tests if <em>title</em> ends with the specified <code>suffix</code>.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 * 
+	 * @param suffix
+	 *            the <em>title</em> <code>suffix</code>
+	 */
+	public static void assertTitleEndsWith(WebDriver driver, String suffix) {
+		String title = driver.getTitle();
+
+		assertTrue("The title <" + title + "> doesn't end with <" + suffix + ">", title.endsWith(suffix));
+	}
+
+	/**
+	 * Tests if <em>title</em> ends with the specified <code>suffix</code> ignoring case.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 * 
+	 * @param suffix
+	 *            the <em>title</em> <code>suffix</code>
+	 */
+	public static void assertTitleEndsWithIgnoreCase(WebDriver driver, String suffix) {
+		String title = driver.getTitle().toLowerCase();
+		suffix = suffix.toLowerCase();
+
+		assertTrue("The title <" + title + "> doesn't end with <" + suffix + ">", title.endsWith(suffix));
+	}
+
+	/**
+	 * Tests if <em>title</em> does not end with the specified <code>suffix</code>.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 * 
+	 * @param suffix
+	 *            the <em>title</em> <code>suffix</code>
+	 */
+	public static void assertTitleDoesNotEndWith(WebDriver driver, String suffix) {
+		String title = driver.getTitle();
+
+		assertFalse("The title <" + title + "> ends with <" + suffix + ">", title.endsWith(suffix));
+	}
+
+	/**
+	 * Tests if <em>title</em> does not end with the specified <code>suffix</code> ignoring case.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 * 
+	 * @param suffix
+	 *            the <em>title</em> <code>suffix</code>
+	 */
+	public static void assertTitleDoesNotEndWithIgnoreCase(WebDriver driver, String suffix) {
+		String title = driver.getTitle().toLowerCase();
+		suffix = suffix.toLowerCase();
+
+		assertFalse("The title <" + title + "> ends with <" + suffix + ">", title.endsWith(suffix));
+	}
+
+	/**
+	 * Tests if <em>title</em> contains the <code>sequence</code>.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 * 
+	 * @param sequence
+	 *            the <code>sequence</code> to search for
+	 */
+	public static void assertTitleContains(WebDriver driver, String sequence) {
+		String title = driver.getTitle();
+
+		assertTrue("The title <" + title + "> does not contain <" + sequence + ">", title.contains(sequence));
+	}
+
+	/**
+	 * Tests if <em>title</em> contains the <code>sequence</code> ignoring case.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 * 
+	 * @param sequence
+	 *            the <code>sequence</code> to search for
+	 */
+	public static void assertTitleContainsIgnoreCase(WebDriver driver, String sequence) {
+		String title = driver.getTitle().toLowerCase();
+		sequence = sequence.toLowerCase();
+
+		assertTrue("The title <" + title + "> does not contain <" + sequence + ">", title.contains(sequence));
+	}
+
+	/**
+	 * Tests if <em>title</em> does not contain the <code>sequence</code>.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 * 
+	 * @param sequence
+	 *            the <code>sequence</code> to search for
+	 */
+	public static void assertTitleDoesNotContain(WebDriver driver, String sequence) {
+		String title = driver.getTitle();
+
+		assertFalse("The title <" + title + "> contains <" + sequence + ">", title.contains(sequence));
+	}
+
+	/**
+	 * Tests if <em>title</em> does not contain the <code>sequence</code>.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 * 
+	 * @param sequence
+	 *            the <code>sequence</code> to search for
+	 */
+	public static void assertTitleDoesNotContainIgnoreCase(WebDriver driver, String sequence) {
+		String title = driver.getTitle().toLowerCase();
+		sequence = sequence.toLowerCase();
+
+		assertFalse("The title <" + title + "> contains <" + sequence + ">", title.contains(sequence));
+	}
+
+	/**
+	 * Tests if <em>title</em> matches the given <code>regex</code>.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 * 
+	 * @param regex
+	 *            the regular expression to which this string is to be matched
+	 */
+	public static void assertTitleMatches(WebDriver driver, String regex) {
+		String title = driver.getTitle();
+
+		assertTrue("The title <" + title + "> does not match <" + regex + ">", title.matches(regex));
+	}
+
+	/**
+	 * Tests if <em>title</em> does not match the given <code>regex</code>.
+	 * 
+	 * @param driver
+	 *            the <code>driver</code> where will be checked
+	 * 
+	 * @param regex
+	 *            the regular expression to which this string is to be matched
+	 */
+	public static void assertTitleDoesNotMatch(WebDriver driver, String regex) {
+		String title = driver.getTitle();
+
+		assertFalse("The title <" + title + "> matches <" + regex + ">", title.matches(regex));
 	}
 
 	/**
